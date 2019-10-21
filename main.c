55a872a3b5b992dd51ddb637743870704b1c7877
Inclusão Digital <inclusaodigital@A101-40TL.imd.local>
2019-10-21 11:58:46 -0300

    Adicionando leitura de um arquivo .txt

--- main.c
+++ main.c
@@ -6,6 +6,33 @@
 #include <stdio.h>
 
 int main() {
+
+	//ler arq
+	FILE *arq;
+	
+	char Linha[100];
+	char *result;
+	int i;
+
+  	clrscr();
+
+	// Abre um arquivo TEXTO para LEITURA
+	arq = fopen("ArqTeste.txt", "rt");
+	if (arq == NULL) {// Se houve erro na abertura
+		printf("Problemas na abertura do arquivo\n");
+		return;
+	}
+	i = 1;
+	// Ler cada linha do arquivo
+	while (!feof(arq)){ 	
+		result = fgets(Linha, 100, arq); 
+		if (result) 
+		printf("Linha %d : %s",i,Linha);
+		i++;
+	}
+	fclose(arq);
+
+	//criar img
 	char lixo, c = 'A';
 	Desenho d = criaDesenho();
 	while (c != 'S') {
