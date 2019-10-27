285debccf07bdeccc66923c897c164c19106845f
Sousa <jose.sousa1251@gmail.com>
2019-10-26 21:09:43 -0300

    Lendo arquivo txt

--- main.c
+++ main.c
@@ -1,11 +1,6 @@
-#include "desenho.h"
-#include "circulo.h"
-#include "quadrado.h"
-#include "ponto.h"
-
 #include <stdio.h>
 
-int main() {
+void main() {
 
 	//ler arq
 	FILE *arq;
@@ -13,8 +8,6 @@ int main() {
 	char Linha[100];
 	char *result;
 	int i;
-
-  	clrscr();
 
 	// Abre um arquivo TEXTO para LEITURA
 	arq = fopen("ArqTeste.txt", "rt");
@@ -32,55 +25,5 @@ int main() {
 	}
 	fclose(arq);
 
-	//criar img
-	char lixo, c = 'A';
-	Desenho d = criaDesenho();
-	while (c != 'S') {
-		printf("\nDigite o que deseja fazer: \n");
-		printf("C - inserir circulo\n");
-		printf("Q - inserir quadrado\n");
-		printf("P - inserir ponto\n");
-		printf("A - calcular área total\n");
-		printf("S - sair do programa\n");
-		printf("OLA MUNDO!\n");
-		scanf("%c", &c);
-		if (c == 'C') {
-			int raio, x, y;
-			printf("Digite o raio: \n");
-			scanf("%d", &raio);
-			printf("Digite o centro na forma x y: \n");
-			scanf("%d %d", &x, &y);
-
-			Ponto p = definePonto(x, y);
-			Circulo c = defineCirculo(raio, p);
-			insereCirculo(&d, c);
-		}
-		else if (c == 'Q') {
-			int lado, x, y;
-			printf("Digite o lado: \n");
-			scanf("%d", &lado);
-			printf("Digite o topo esquerdo na forma x y: \n");
-			scanf("%d %d", &x, &y);
-
-			Ponto p = definePonto(x, y);
-			Quadrado q = defineQuadrado(lado, p);
-			insereQuadrado(&d, q);
-		}
-		else if (c == 'P') {
-			int x, y;
-			printf("Digite o ponto na forma x y: \n");
-			scanf("%d %d", &x, &y);
-
-			Ponto p = definePonto(x, y);
-			inserePonto(&d, p);
-			
-		}
-		else if (c == 'A') {
-			float area = areaTotal(d);
-			int n = numeroFormas(d);
-			printf("Existem %d formas no desenho ocupando %f de área\n",n, area);
-		}
-		scanf("%c", &lixo);
-	}
-	return 0;
+	
 }
