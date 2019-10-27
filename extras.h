c98683bb9ae90dd49206e704d703b709fc170fa9
Alexandra Silva <40324120+fatiguebottle@users.noreply.github.com>
2019-10-27 16:33:11 -0300

    Headers e default.c
    tirando as headers, os arquivos C tão vazios por enquanto. Dá uma olhada etc.

---
+++ extras.h
@@ -0,0 +1,6 @@
+#ifndef EXTRAS_H
+#define EXTRAS_H
+
+//extra functions
+//as funcoes extras (que a gente não vai fazer agora)
+#endif
---
+++ structs.h
@@ -0,0 +1,46 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+typedef struct image{
+    char nome[8];
+    int valores[2];
+} image;
+
+typedef struct clear{
+    char nome[8];
+    int valores[3];
+} clear;
+
+typedef struct color{
+    char nome[8];
+    int valores[3];
+} color;
+
+typedef struct line{
+    char nome[8];
+    int valores[4];
+} line;
+
+//polygon tem uma qtd N de pontos e seus respectivos pontos (x,y)
+typedef struct polygon{
+    char nome[8];
+    int n; 
+    int valores[2*n]; 
+} polygon;
+
+typedef struct circle{
+    char nome[8];
+    int valores[3];
+} circle;
+
+typedef struct fill{
+    color cor; 
+    char nome[8];
+    int valores[2];
+} fill;
+
+typedef struct save{
+    FILE arq;
+} save;
+
+#endif
