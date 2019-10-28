9bd6c8250a1ef8b2c66e91a93d5e3fb2dbacc4f6
Alexandra Silva <40324120+fatiguebottle@users.noreply.github.com>
2019-10-28 14:20:47 -0300

    Atualizando structs.h
    Adicionei area em struct Fill e trocei o nome das variáveis pra inglês

--- structs.h
+++ structs.h
@@ -2,45 +2,45 @@
 #define STRUCTS_H
 
 typedef struct options{
-    char nome[8];
+    char name[8];
 } options;
 
 typedef struct image{
-    char nome[8];
-    int valores[2];
+    char name[8];
+    int values[2];
 } image;
 
 typedef struct clear{
-    char nome[8];
-    int valores[3];
+    char name[8];
+    int values[3];
 } clear;
 
 typedef struct color{
-    char nome[8];
-    int valores[3];
+    char name[8];
+    int values[3];
 } color;
 
 typedef struct line{
-    char nome[8];
-    int valores[4];
+    char name[8];
+    int values[4];
 } line;
 
-//polygon tem uma qtd N de pontos e seus respectivos pontos (x,y)
 typedef struct polygon{
-    char nome[8];
+    char name[8];
     int n; 
-    int valores[2*n]; 
+    int values[2*n]; 
 } polygon;
 
 typedef struct circle{
-    char nome[8];
-    int valores[3];
+    char name[8];
+    int values[3];
 } circle;
 
 typedef struct fill{
-    color cor; 
-    char nome[8];
-    int valores[2];
+    color Color; 
+    char name[8];
+    int values[2];
+    int area;
 } fill;
 
 typedef struct save{
