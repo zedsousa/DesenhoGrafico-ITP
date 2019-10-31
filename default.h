f546ecd43e9954bffffadf0e2bd712db0cabece4
Alexandra Silva <40324120+fatiguebottle@users.noreply.github.com>
2019-10-31 08:58:34 -0300

    Atualizando default.h
    Eu trocei os parametros com (int x,int y) pra (point P) sendo point uma struct com int x e int y, só pra ficar mais organizado pra passar pra essas funções mesmo.

--- default.h
+++ default.h
@@ -1,15 +1,16 @@
 #ifndef DEFAULT_H
 #define DEFAULT_H
+#include "structs.h"
 
 //default functions
 //são funções considerando as opções do exemplo lá 
-void makeImage(int line,int column);
-int cleanImage(int **m);
-int colorImage(int r,int g,int b);
-int makeLine(int x1,int y1,int x2,int y2);
-int makeCircle(int x,int y,int radius);
-int makePolygon(int n,int *a);
-int fillForm(int x,int y);
+void makeImage(point P);
+int cleanImage(color C);
+int colorImage(color C);
+int makeLine(point *P); //point P[2]
+int makeCircle(point P,int radius);
+int makePolygon(int n,point *P); //point P[n]
+int fillForm(point P);
 int saveImage(FILE *arq);
 
 #endif
