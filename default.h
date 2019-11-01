47d67d12ce63b90f65abb09fe9beab43e71694fd
Alexandra Silva <40324120+fatiguebottle@users.noreply.github.com>
2019-11-01 11:43:11 -0300

    add rect e remove array de polygon

--- default.h
+++ default.h
@@ -9,7 +9,8 @@ int cleanImage(color C);
 int colorImage(color C);
 int makeLine(point *P); //point P[2]
 int makeCircle(point P,int radius);
-int makePolygon(int n,point *P); //point P[n]
+int makePolygon(int n); //point P[n]
+int makeRect(int x,int y);
 int fillForm(point P);
 int saveImage(FILE *arq);
 
