e7133705912c1cb0f0c8ce931ed73f57adcb2600
Alexandra Silva <40324120+fatiguebottle@users.noreply.github.com>
2019-11-05 22:46:14 -0300

    Update default.h

--- default.h
+++ default.h
@@ -11,7 +11,7 @@ void setPixel(int x,int y,int w,int h,color (*C)[w],color Color);
 void makeLine(point a,point b,int w,int h,color (*C)[w]);
 void makeCircle(point P,int radius);
 void makePolygon(int n,point *P,int w,int h,color (*C)[w]);
-void makeRect(Point p,int height,int width,int w,int h,color (*C)[w]);
+void makeRect(point p,int height,int width,int w,int h,color (*C)[w]);
 void fillForm(point P);
 void saveImage(FILE *arq);
 
